de04eb85193ba9af0d6b53658a0a59648b2b77e3
Azdin Mediri <a.elmediri@gmail.com>
2023-12-14 21:07:15 +0100

    mur exterieur

--- maze.py
+++ maze.py
@@ -7,18 +7,28 @@ class Maze:
         self.rows = rows
         self.cols = cols
         self.maze = np.zeros((rows, cols), dtype=int)
-    
+
+        # Initialisation des murs exterieurs du labyrinthe avec l'entrée et la sortie
+        for i in range(rows):
+            for j in range(cols):
+                if (i == 0 or i == rows-1 or j == 0 or j == cols-1) and ((i,j) != (0,1) and (i,j) != (rows-1, cols-2)):
+                    self.maze[i, j] = 1
+
 
     def pyplot(self):
+        """ Affichage du labyrinthe """
         fig, ax = plt.subplots()
         ax.imshow(self.maze, cmap='binary')
         ax.set_xticks([])
         ax.set_yticks([])
         plt.show()
 
-
-
-laby = Maze(10, 10)
-laby.pyplot()
-
+    def main_droite(self):
+        """ Algorithme de la main droite """
         
+        pass
+
+
+
+laby = Maze(40, 40)
+laby.pyplot()
